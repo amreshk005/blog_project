ace7abad3566fb1b54ab11f6f0bdede88bf7b8e5
amreshk005 <amreshk005@gmail.com>
2019-04-03 18:16:40 +0530

    user auth update

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,7 +8,7 @@ app_name = 'blog'
 urlpatterns = [
    
     path('', views.post_list,name='post_list'),
-    path('<id>/<slug>/',views.post_detail,name='post_detail'),
+    path('/<slug>/<id>/',views.post_detail,name='post_detail'),
     path('post_create/',views.post_create, name="post_create"),
     path('user_login/',views.user_login,name='user_login'),
     path('user_logout/',views.user_logout,name='user_logout'),
