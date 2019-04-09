b2e2bec7b355962510990bcc1c45411c09bacd0a
amreshk005 <amreshk005@gmail.com>
2019-04-09 16:34:59 +0530

    changed sql database to mysql

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -90,10 +90,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER':'root',
+        'PASSWORD':'password',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'OPTIONS':{
+                'sql_mode':'traditional',
+        }
     }
 }
 
